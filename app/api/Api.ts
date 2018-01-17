a75be73cff2f0a91dabffc24572a8f804d18676e
sofi <Darkf0x13566@gmail.com>
2018-01-18 00:10:04 +0300

    api added

---
+++ app/api/Api.ts
@@ -0,0 +1,46 @@
+import rp from 'request-promise-native';
+import {API_CORE} from './env';
+
+export function getUsers() {
+    const options = {
+        method: 'GET',
+        uri: `${API_CORE}/users/10`,
+        json: 'true' 
+    }
+
+    try {
+        return rp(options)
+    } catch(e) {
+        console.log('err', e)
+    }
+}
+
+export function getAlbums(userId: number) {
+    const options = {
+        method: 'GET',
+        uri: `${API_CORE}/albums`,
+        json: 'true',
+        params: {userId} 
+    }
+
+    try {
+        return rp(options)
+    } catch(e) {
+        console.log('err', e)
+    }
+}
+
+export function getAlbum(albumId: number) {
+    const options = {
+        method: 'GET',
+        uri: `${API_CORE}/photos`,
+        json: 'true',
+        params: {albumId} 
+    }
+
+    try {
+        return rp(options)
+    } catch(e) {
+        console.log('err', e)
+    }
+}
